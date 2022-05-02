b54ead6a1f01a3b6c393c278cb9d7453293a736b
Uzo Ogbonna <younguzii@yahoo.com>
2022-05-02 05:23:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "external" "external_provider" {
-  program = ["python3", "malicious_script.py"]
+  program = ["python", "${path.module}/bad_python.py"]
 }
 
 output "external_provider_example" {
